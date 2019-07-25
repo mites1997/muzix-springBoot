b457e3e4a9c24e0b6f95cac8fdcefe1b566542c2
mites1997 <arnabfeb15@gmail.com>
2019-07-25 07:21:12 +0530

    all getters setters constructors removed due to lombok

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -1,10 +1,16 @@
 package com.stackroute.domain;
 
-import lombok.Data;
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Track {
 
     @Id
@@ -12,45 +18,5 @@ public class Track {
     String name;
     String comment;
 
-    public Track() {
-    }
 
-    public Track(int id, String name, String comment) {
-        this.id = id;
-        this.name = name;
-        this.comment = comment;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", comment='" + comment + '\'' +
-                '}';
-    }
 }
