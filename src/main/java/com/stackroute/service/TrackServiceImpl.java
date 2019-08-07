1e10b0614842961304fa49f5c5a6a3b23039d74d
mites1997 <51950018+mites1997@users.noreply.github.com>
2019-08-07 12:40:05 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/service/TrackServiceImpl.java
@@ -38,11 +38,16 @@ public class TrackServiceImpl implements TrackService {
     }
 
     @Override
-    public void deleteTrack(int id) {
-
-        trackRepository.delete(getTrackById(id));
+    public List<Track> deleteTrack(int id) {
+          if(!trackRepository.existsById(id))
+        {
+            throw new TrackNotFoundException("Track not found");
+        }
+      trackRepository.delete(getTrackById(id));
+        return trackRepository.findAll();
 
     }
+
 
     @Override
     public Track updateTrack(int id,String comment) {
