333e5577c6e31bfd6c8095345802eaa923fb2133
mites1997 <51950018+mites1997@users.noreply.github.com>
2019-08-07 06:06:10 +0530

    Update Track.java

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -14,9 +14,9 @@ import javax.persistence.Id;
 public class Track {
 
     @Id
-    int id;
-    String name;
-    String comment;
+  private  int id;
+   private String name;
+   private String comment;
 
 
 }
